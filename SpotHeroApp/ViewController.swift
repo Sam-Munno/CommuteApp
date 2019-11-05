b42526297af424301b37100cd0edba0e9d1dc9ac
Sam Munno <samuel.munno@united.com>
2019-11-04 18:57:14 -0600

    Initial Commit

---
+++ SpotHeroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotHeroApp
+//
+//  Created by Sam Munno on 11/4/19.
+//  Copyright © 2019 Sam Munno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
